c8e21f48728b1d1b188c1ec586dded91245aba88
ericaj <ericaj.vieira13@gmail.com>
2024-08-30 22:50:36 -0300

    style: fix style

--- src/services/confirmation.service.ts
+++ src/services/confirmation.service.ts
@@ -7,22 +7,22 @@ export class ConfirmationService {
   constructor(private prisma: PrismaService) {}
 
   async confirmMeasure(measureConfirmationData: MeasureConfirmationBodyDto) {
-    const uuid = measureConfirmationData.measure_id
+    const uuid = measureConfirmationData.measure_id;
     const existingMeasure = await this.prisma.measure.findUnique({
       where: { uuid }
-    })
+    });
 
     if (!existingMeasure) {
-      throw new HttpException('Leitura não encontrada', HttpStatus.NOT_FOUND)
+      throw new HttpException('Leitura não encontrada', HttpStatus.NOT_FOUND);
     }
 
     if (existingMeasure.hasConfirmed) {
-      throw new HttpException('Leitura do mês já realizada', HttpStatus.CONFLICT)
+      throw new HttpException('Leitura do mês já realizada', HttpStatus.CONFLICT);
     }
 
     await this.prisma.measure.update({
       where: { uuid },
       data: { hasConfirmed: true, value: measureConfirmationData.confirmed_value }
-    })
+    });
   }
 }

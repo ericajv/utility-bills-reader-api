83069a375a58c5eb2d0c1598138df7370541f5a6
ericaj <ericaj.vieira13@gmail.com>
2024-08-30 22:50:19 -0300

    refactor: improve customer measures controller

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,31 +1,16 @@
-import { Controller, Get, HttpException, HttpStatus, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { CustomerMeasuresQueryDto } from 'src/dto/customer_measures_query.dto';
-import { PrismaService } from 'src/services/prisma.service';
+import { CustomerService } from 'src/services/customer.service';
 
 @Controller('/:customer_code')
 export class CustomerController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly customerService: CustomerService) {}
 
   @Get('/list')
   async listMeasures(
     @Param('customer_code') customerCode: string,
     @Query() query: CustomerMeasuresQueryDto
   ) {
-    const measures = await this.prisma.measure.findMany({ 
-      where: query.measure_type ? { customerCode, ...query } : { customerCode },
-      select: {
-        uuid: true,
-        datetime: true,
-        type: true,
-        hasConfirmed: true,
-        imageUrl: true
-      }
-    });
-
-    if (measures.length === 0) {
-      throw new HttpException('Nenhuma leitura não encontrada', HttpStatus.NOT_FOUND)
-    }
-
-    return { customer_code: customerCode, measures }
+    return this.customerService.getCustomerMeasures(customerCode, query.measure_type);
   }
 }
--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { MeasureType } from '@prisma/client';
 
 @Injectable()
 export class CustomerService {
-  getHello(): object {
-    return { hello: 'Hello World!' };
+  constructor(private prisma: PrismaService) {}
+
+  async getCustomerMeasures(customerCode: string, measureType: MeasureType|null) {
+    const measures = await this.prisma.measure.findMany({ 
+      where: measureType ? { customerCode, type: measureType } : { customerCode },
+      select: {
+        uuid: true,
+        datetime: true,
+        type: true,
+        hasConfirmed: true,
+        imageUrl: true
+      }
+    });
+
+    if (measures.length === 0) {
+      throw new HttpException('Nenhuma leitura encontrada', HttpStatus.NOT_FOUND)
+    }
+
+    return { customer_code: customerCode, measures }
   }
 }
